673334410082ae3c2e1bccfefcdb540542e90913
Dominic Gadoury <dominicgadoury@Dominics-iMac.local>
2017-05-10 10:48:23 -0600

    Initial Commit

---
+++ xCode8Git/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  xCode8Git
+//
+//  Created by Dominic Gadoury on 5/10/17.
+//  Copyright © 2017 Dominic Gadoury. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
